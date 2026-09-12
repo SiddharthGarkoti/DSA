acde5c0d1b3b29e8295cccbb9808458a8ec26219
Siddharth Garkoti <siddharthgarkoti4545@gmail.com>
2026-09-12 06:09:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (88.02%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -36,16 +36,8 @@ public:
             }
             head=head->next;
         }
-        while(l1){
-            head->next=l1;
-            l1=l1->next;
-            head=head->next;
-        }
-        while(l2){
-            head->next=l2;
-            l2=l2->next;
-            head=head->next;
-        }
+        if(!l1) head->next=l2;
+        else head->next=l1;
         return newHead;
     }
 };
